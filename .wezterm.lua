a77234346f2fea2455daf3a4f9ea8b0f9024e018
Rick Waterman <rwaterman@users.noreply.github.com>
2025-10-03 16:49:15 -0700

    chore: wezterm natural text editing plus zshrc config and cleanup

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,13 +1,32 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This will hold the configuration.
+local act = wezterm.action
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+-- Colors
+-- config.color_scheme = 'One Dark (Gogh)'
+config.color_scheme = 'Tango (terminal.sexy)'
+-- Fonts
+config.font = wezterm.font({
+  family = "MesloLGSDZ Nerd Font",
+  weight = "Regular",
+})
+config.font_size = 14
+config.line_height = 1.2
 
--- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+-- Natural Text Editing 
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = false
 
--- and finally, return the configuration to wezterm
+config.keys = {
+-- Natural Text Editing
+  { mods = "OPT", key = "LeftArrow",  action = act.SendKey({ mods = "ALT",  key = "b" }) },
+  { mods = "OPT", key = "RightArrow", action = act.SendKey({ mods = "ALT",  key = "f" }) },
+  { mods = "CMD", key = "LeftArrow",  action = act.SendKey({ mods = "CTRL", key = "a" }) },
+  { mods = "CMD", key = "RightArrow", action = act.SendKey({ mods = "CTRL", key = "e" }) },
+  { mods = "OPT", key = "Backspace",  action = act.SendKey({ mods = "ALT",  key = "Backspace" }) },
+  { mods = "OPT", key = "Delete",     action = act.SendKey({ mods = "ALT",  key = "d" }) },
+  { mods = "CMD", key = "Backspace",  action = act.SendKey({ mods = "CTRL", key = "u" }) },
+  { mods = "CMD", key = "Delete",     action = act.SendKey({ mods = "CTRL", key = "k" }) },
+}
+
 return config
